d5c868beb205269c2abbbe081d38d7b7f0df1bec
Tevfik Tan Onay <tevfiktanonay@gmail.com>
2025-11-26 15:31:27 +0000

    created models for sales, product, ship sources

---
+++ models/stg_raw__product.sql
@@ -0,0 +1,11 @@
+with
+ source as (
+  select * from {{ source('raw', 'product') }}
+ ),
+ renamed as (
+  select
+   products_id,
+   CAST(purchSE_PRICE as FLOAT64) as purchase_price
+  from source
+ )
+select * from renamed
